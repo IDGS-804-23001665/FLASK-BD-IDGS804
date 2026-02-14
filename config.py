ccd8020bbb8018ef521d6e98d24639a47c192082
Juan Daniel Nachez <juandanielnachezhernandez@gmail.com>
2026-02-14 09:38:43 -0600

    Lista de alumnos 14/02/2026

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+from sqlalchemy import create_engine
+
+class Config(object):
+    SECRET_KEY="ClaveSecreta"
+    SESSION_COOKIE_SECURE=False
+    
+class DevelopmentConfig(Config):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@localhost/bdidgs804'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
---
+++ forms.py
@@ -0,0 +1,21 @@
+from wtforms import Form, StringField, DateField, IntegerField, EmailField,validators
+from flask_wtf import FlaskForm
+ 
+class UserForm(Form):
+    id = IntegerField('id',[
+        validators.number_range(min=1, max=20, message='valor no valido')
+    ])
+   
+    nombre = StringField('nombre',[
+        validators.DataRequired(message='El nombre es requerido'),
+        validators.length(min=4, max=20, message='requiere minimo 4, maximo 20')
+    ])
+   
+    apaterno = StringField('apaterno',[
+        validators.DataRequired(message='El apellido es requerido')
+    ])
+   
+    email = EmailField('correo',[
+        validators.DataRequired(message='El apellido es requerido'),
+        validators.Email(message='Ingrese un correo valido')
+    ])
